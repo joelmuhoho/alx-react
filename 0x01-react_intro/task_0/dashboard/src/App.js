315ce83c4c34ba4c54c178a9a18ff74c2dcc6264
joekariuki3 <joekariuki3@gmail.com>
2025-11-26 12:14:46 +0300

    feat: initialize React dashboard project with essential files and structure
    - Added package.json with dependencies and scripts for React application
    - Created index.html as the main HTML file for the application
    - Added App.css for styling the application components
    - Implemented App.js as the main component with header, body, and footer
    - Included favicon and logo images for branding
    - Created index.css to import App.css
    - Set up index.js to render the App component

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./alx-logo.jpg";
+
+const App = () => {
+  return (
+    <>
+      <div className="App-header">
+        <img className="App-logo" src={logo} alt="alx holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright 2020 - ALX</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
