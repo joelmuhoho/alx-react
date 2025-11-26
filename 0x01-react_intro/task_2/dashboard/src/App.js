40e1aac240f70352860e1d3edde71c0ad95cf18a
joekariuki3 <joekariuki3@gmail.com>
2025-11-26 13:40:02 +0300

    feat: add login form with email and password inputs

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -10,6 +10,21 @@ const App = () => {
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          placeholder="enter your email"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="enter your password"
+        />
+        <button>OK</button>
       </div>
       <div className="App-footer">
         <p>
