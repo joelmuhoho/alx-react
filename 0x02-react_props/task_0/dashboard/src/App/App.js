c57432d538d42bc6ff7c51aa97c93b49ee12e400
joekariuki3 <joekariuki3@gmail.com>
2025-12-02 23:06:56 +0300

    feat: implement login logic in App component and update tests for CourseList rendering

--- 0x02-react_props/task_0/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -2,20 +2,24 @@ import Notifications from "../Notifications/Notifications";
 import Header from "../Header/Header";
 import Login from "../Login/Login";
 import Footer from "../Footer/Footer";
+import CourseList from "../CourseList/CourseList";
+import PropTypes from "prop-types";
 
-const App = () => {
+const App = ({ isLoggedIn = false }) => {
   return (
     <>
-      <Notifications />
+      <Notifications displayDrawer={false} />
       <div className="App">
         <Header />
         <main className="App-body">
-          <Login />
+          {isLoggedIn ? <CourseList /> : <Login />}
         </main>
         <Footer />
       </div>
     </>
   );
 };
+
+App.propTypes = { isLoggedIn: PropTypes.bool };
 
 export default App;
--- 0x02-react_props/task_0/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -7,12 +7,11 @@ describe("App", () => {
     render(<App />);
   });
 
-  it("renders Notification component that has a paragraph 'Here is the list of notifications' text ", () => {
+  it("renders Notification component that has a paragraph 'Your notifications' text ", () => {
     render(<App />);
-    const notificationParagraphElement = screen.getByText(
-      "Here is the list of notifications"
-    );
+    const notificationParagraphElement = screen.getByText("Your notifications");
     expect(notificationParagraphElement).toBeInTheDocument();
+    expect(notificationParagraphElement.parentElement).toHaveClass("menuItem");
   });
 
   it("renders Header component that has a header element with class App-header", () => {
@@ -36,5 +35,21 @@ describe("App", () => {
     const footerElement = screen.getByRole("contentinfo");
     expect(footerElement).toBeInTheDocument();
     expect(footerElement).toHaveClass("App-footer");
+  });
+});
+
+describe("App when isLoggedIn is true", () => {
+  it("renders CourseList component with a table element", () => {
+    render(<App isLoggedIn={true} />);
+    const tableElement = screen.getByRole("table");
+    expect(tableElement).toBeInTheDocument();
+  });
+
+  it("does not render Login Component", () => {
+    render(<App isLoggedIn={true} />);
+    const loginElementParagraph = screen.queryByText(
+      "Login to access the full dashboard"
+    );
+    expect(loginElementParagraph).toBe(null);
   });
 });
