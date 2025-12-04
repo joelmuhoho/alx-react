387e6215b29112d780d4849a713b3bbf60319791
joelmuhoho <joekariuki3@gmail.com>
2025-12-04 16:07:11 +0300

    refactor: convert Notification component to class-based and improve prop handling

--- 0x03-React_component/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x03-React_component/task_2/dashboard/src/Notifications/Notifications.js
@@ -5,51 +5,66 @@ import PropTypes from "prop-types";
 import NotificationItemShape from "../NotificationItem/NotificationItemShape";
 // import closeIcon from "./close-icon.png";
 
-const Notification = ({ displayDrawer = false, listNotifications = [] }) => {
-  function buttonClicked() {
+class Notification extends React.Component {
+  constructor(props) {
+    super(props);
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+  buttonClicked() {
     console.log("Close button has been clicked");
   }
-  return (
-    <>
-      <div className="menuItem">
-        <p>Your notifications</p>
-      </div>
-      {displayDrawer ? (
-        <div className="Notifications">
-          {listNotifications.length === 0 ? (
-            <p>No new notification for now</p>
-          ) : (
-            <>
-              <p>Here is the list of notifications</p>
-              <ul>
-                {listNotifications.map((notification) => {
-                  return (
-                    <NotificationItem
-                      key={notification.id}
-                      type={notification.type}
-                      html={notification.html}
-                      value={notification.value}
-                    />
-                  );
-                })}
-              </ul>
-            </>
-          )}
-          <button
-            style={{ align: "right", ariaLabel: "Close" }}
-            onClick={buttonClicked}
-            className="Notifications-close-button"
-          >
-            {/* <img src={closeIcon} alt="close icon"/> */}X
-          </button>
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+  render() {
+    return (
+      <>
+        <div className="menuItem">
+          <p>Your notifications</p>
         </div>
-      ) : (
-        ""
-      )}
-    </>
-  );
+        {this.props.displayDrawer ? (
+          <div className="Notifications">
+            {this.props.listNotifications.length === 0 ? (
+              <p>No new notification for now</p>
+            ) : (
+              <>
+                <p>Here is the list of notifications</p>
+                <ul>
+                  {this.props.listNotifications.map((notification) => {
+                    return (
+                      <NotificationItem
+                        key={notification.id}
+                        id={notification.id}
+                        type={notification.type}
+                        html={notification.html}
+                        value={notification.value}
+                        markAsRead={this.markAsRead}
+                      />
+                    );
+                  })}
+                </ul>
+              </>
+            )}
+            <button
+              style={{ align: "right", ariaLabel: "Close" }}
+              onClick={this.buttonClicked}
+              className="Notifications-close-button"
+            >
+              {/* <img src={closeIcon} alt="close icon"/> */}X
+            </button>
+          </div>
+        ) : (
+          ""
+        )}
+      </>
+    );
+  }
+}
+Notification.defaultProps = {
+  displayDrawer: false,
+  listNotifications: [],
 };
-Notification.prototype = {
+Notification.protoType = {
   displayDrawer: PropTypes.bool,
   listNotifications: PropTypes.arrayOf(NotificationItemShape),
 };
