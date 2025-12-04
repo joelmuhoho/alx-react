3442810b516fa4d77b808ec93ec9189b6152e141
joelmuhoho <joekariuki3@gmail.com>
2025-12-04 16:07:49 +0300

    test: add click event test for NotificationItem to verify markAsRead functionality

--- 0x03-React_component/task_2/dashboard/src/NotificationItem/NotificationItem.test.js
+++ 0x03-React_component/task_2/dashboard/src/NotificationItem/NotificationItem.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 import NotificationItem from "./NotificationItem";
 
 describe("NotificationItem", () => {
@@ -37,5 +38,15 @@ describe("NotificationItem", () => {
     );
     expect(listItemElement).toHaveAttribute("value", "test");
     expect(listItemElement).toHaveTextContent("test");
+  });
+
+  it("make sure NotificationItem component when clicked it calls markAsRead property with right ID", async () => {
+    const notificationId = 7;
+    const markAsReadSpy = jest.fn(); // Mock function
+    render(<NotificationItem markAsRead={markAsReadSpy} id={notificationId} />);
+    const user = userEvent.setup();
+    const notification = screen.getByRole("listitem");
+    await user.click(notification); // Simulate click event
+    expect(markAsReadSpy).toHaveBeenCalledWith(notificationId);
   });
 });
