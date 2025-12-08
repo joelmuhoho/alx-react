19fc4627ab82376172a593b4713076e0c51d4aae
joelmuhoho <joekariuki3@gmail.com>
2025-12-08 22:43:18 +0300

    style: add responsive styles to NotificationItem component

--- 0x04-React_inline_styling/task_3/dashboard/src/NotificationItem/NotificationItem.js
+++ 0x04-React_inline_styling/task_3/dashboard/src/NotificationItem/NotificationItem.js
@@ -5,13 +5,23 @@ import { StyleSheet, css } from "aphrodite";
 const styles = StyleSheet.create({
   default: { color: "blue" },
   urgent: { color: "#e0354b" },
+  NotificationItem: {
+    "@media (max-width: 900px)": {
+      width: "100%",
+      borderBottom: "1px solid black",
+      fontSize: "20px",
+      padding: "10px 8px",
+      listStyle: "none",
+    },
+  },
 });
 class NotificationItem extends PureComponent {
   render() {
     return (
       <li
         className={css(
-          this.props.type === "default" ? styles.default : styles.urgent
+          this.props.type === "default" ? styles.default : styles.urgent,
+          styles.NotificationItem
         )}
         data-notification-type={this.props.type}
         value={this.props.value}
