4513b3d48eb98e6bf49347de76205b680d45f855
joelmuhoho <joekariuki3@gmail.com>
2025-12-08 17:57:52 +0300

    test: add spacing for improved readability in CourseList tests

--- 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseList.test.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseList.test.js
@@ -2,10 +2,12 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import CourseList from "./CourseList";
 import { StyleSheetTestUtils } from "aphrodite";
+
 const listCourses = [
   { id: 1, name: "ES6", credit: 60 },
   { id: 2, name: "Webpack", credit: 20 },
 ];
+
 describe("CourseList", () => {
   beforeEach(() => {
     StyleSheetTestUtils.suppressStyleInjection();
@@ -18,11 +20,13 @@ describe("CourseList", () => {
   it("renders without crashing", () => {
     render(<CourseList />);
   });
+
   it("renders correctly if listCourses(array of courses) is not passed", () => {
     render(<CourseList />);
     const messageElement = screen.getByText("No course available yet");
     expect(messageElement).toBeInTheDocument();
   });
+
   it("renders correctly if listCourses(empty array of courses) is passed", () => {
     render(<CourseList listCourses={[]} />);
     const messageElement = screen.getByText("No course available yet");
