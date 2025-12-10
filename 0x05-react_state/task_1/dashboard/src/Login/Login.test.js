569fc32df7584e79e1c0c7f778d0482a9fa8abbe
joelmuhoho <joekariuki3@gmail.com>
2025-12-10 12:23:10 +0300

    test: add tests for Login button state and interaction

--- 0x05-react_state/task_1/dashboard/src/Login/Login.test.js
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Login from "./Login";
 import { StyleSheetTestUtils } from "aphrodite";
@@ -28,5 +28,30 @@ describe("Login", () => {
     expect(emailInputElement).toBeInTheDocument();
     expect(pwdLabelElement).toBeInTheDocument();
     expect(pwdInputElement).toBeInTheDocument();
+  });
+
+  it("Should render with Login button disabled by default", () => {
+    render(<Login />);
+    const LoginButton = screen.getByRole("button", { name: /Login/i });
+    expect(LoginButton).toBeInTheDocument();
+    expect(LoginButton).toBeDisabled();
+  });
+  it("Should render with disabled Login button after adding email and password Login Button should be enabled", () => {
+    render(<Login />);
+    const emailInput = screen.getByRole("textbox", { name: /email/i });
+    const passwordInput = screen.getByLabelText(/Password/i);
+    const LoginButton = screen.getByRole("button", { name: /Login/i });
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(LoginButton).toBeInTheDocument();
+    expect(LoginButton).toBeDisabled();
+
+    // enter data
+    fireEvent.change(emailInput, { target: { value: "testuser" } });
+    fireEvent.change(passwordInput, { target: { value: "securepassword123" } });
+
+    // login button should be enabled
+    expect(LoginButton).toBeEnabled();
   });
 });
