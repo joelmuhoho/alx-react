4b1a04ec5a0e2e852927c70ab0eb91e632fc965c
joelmuhoho <joekariuki3@gmail.com>
2025-12-11 16:35:44 +0300

    feat: remove example object and console log from getImmutableObject function

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -8,13 +8,3 @@ import { fromJS } from "./node_modules/immutable/dist/immutable.js";
 const getImmutableObject = (object) => {
   return fromJS(object);
 };
-
-const obj = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132,
-};
-
-const immutableObj = getImmutableObject(obj);
-console.log(immutableObj);
