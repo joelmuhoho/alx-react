727359b612ccf5d0cf0b53c46ba7ff47b2b6cd0f
joelmuhoho <joekariuki3@gmail.com>
2025-12-11 11:41:23 +0300

    test: enhance Header component tests for logout functionality and context handling

--- 0x05-react_state/task_2/dashboard/src/Header/Header.test.js
+++ 0x05-react_state/task_2/dashboard/src/Header/Header.test.js
@@ -1,7 +1,8 @@
 import Header from "./Header";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { StyleSheetTestUtils } from "aphrodite";
+import { AppContext } from "../App/AppContext";
 
 describe("Header", () => {
   beforeEach(() => {
@@ -23,5 +24,66 @@ describe("Header", () => {
 
     expect(imgElement).toBeInTheDocument();
     expect(h1Element).toBeInTheDocument();
+  });
+  it("should reader without logoutSection when default context values are provided", () => {
+    const logOutMock = jest.fn();
+    const user = {
+      email: "",
+      password: "",
+      isLoggedIn: false,
+    };
+
+    const logOut = logOutMock;
+    render(
+      <AppContext Provider value={{ user: user, logOut: logOut }}>
+        <Header />
+      </AppContext>
+    );
+    const logOutSection = screen.queryByText(/Welcome/i);
+    expect(logOutSection).toBe(null);
+  });
+
+  it("should reader with logoutSection when context values are defined", () => {
+    const logOutMock = jest.fn();
+    const user = {
+      email: "test@mail.com",
+      password: "test123",
+      isLoggedIn: true,
+    };
+
+    const logOut = logOutMock;
+    render(
+      <AppContext Provider value={{ user: user, logOut: logOut }}>
+        <Header />
+      </AppContext>
+    );
+    const logOutSection = screen.queryByText(/Welcome test@mail.com/i);
+    expect(logOutSection).toBeInTheDocument();
+  });
+  it("should reader with logoutSection when context values are defined and when logOut is clicked logout function is called", () => {
+    const user = {
+      email: "test@mail.com",
+      password: "test123",
+      isLoggedIn: true,
+    };
+
+    const logOut = jest.fn();
+    const { rerender } = render(
+      <AppContext Provider value={{ user: user, logOut: logOut }}>
+        <Header />
+      </AppContext>
+    );
+    const logOutSection = screen.queryByText(/Welcome test@mail.com/i);
+    const logOutBtn = screen.getByRole("button", { name: /LogOut/i });
+    expect(logOutSection).toBeInTheDocument();
+    expect(logOutBtn).toBeInTheDocument();
+
+    // click on logout
+    fireEvent.click(logOutBtn);
+    expect(logOut).toBeCalled();
+
+    rerender(<Header />);
+    const logOutSection2 = screen.queryByText(/Welcome test@mail.com/i);
+    expect(logOutSection2).toBeNull();
   });
 });
