dfab78cd1527b4a026b9a824b5b5f3fd64cc0da5
joelmuhoho <joekariuki3@gmail.com>
2025-12-11 12:16:31 +0300

    feat: implement user authentication handling in Footer component and tests

--- 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
+++ 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
@@ -1,11 +1,21 @@
 import { getFullYear, getFooterCopy } from "../utils/utils";
 import "./Footer.css";
+import { AppContext } from "../App/AppContext";
+import { useContext } from "react";
 const Footer = () => {
+  const context = useContext(AppContext);
   return (
     <footer className="App-footer">
       <p>
         Copyright {getFullYear()} - {getFooterCopy(true)}
       </p>
+      {context.user.isLoggedIn ? (
+        <p>
+          <a href="#">Contact us</a>
+        </p>
+      ) : (
+        ""
+      )}
     </footer>
   );
 };
