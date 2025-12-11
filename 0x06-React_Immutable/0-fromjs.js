6bd0c1e3de37d1fa3ce8190eca83dc49b04e52e0
joelmuhoho <joekariuki3@gmail.com>
2025-12-11 16:36:30 +0300

    feat: export getImmutableObject function for external usage

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -5,6 +5,6 @@ import { fromJS } from "./node_modules/immutable/dist/immutable.js";
  * @param {Object} object - an object to be converted into an immutable object
  * @returns {Object} - an immutable object
  */
-const getImmutableObject = (object) => {
+export const getImmutableObject = (object) => {
   return fromJS(object);
 };
