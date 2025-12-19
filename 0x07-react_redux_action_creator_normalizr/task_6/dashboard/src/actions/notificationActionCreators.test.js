f9bcc19dedbad49868b60eb3da610d01f5fb95f6
joelmuhoho <joekariuki3@gmail.com>
2025-12-19 14:53:20 +0300

    test: update notification action creators tests to use constants for action types

--- 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/notificationActionCreators.test.js
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/notificationActionCreators.test.js
@@ -3,24 +3,25 @@ import {
   markAsRead,
   setNotificationFilter,
 } from "./notificationActionCreators";
+import { MARK_AS_READ, SET_TYPE_FILTER } from "./notificationActionTypes";
 
 describe("markAsRead", () => {
-  it("Should return expected object with correct type and index as it was called with", () => {
+  it("Should return correct action object when called", () => {
     const expectedIndex = 1;
-    const expectedResults = {
-      type: "MARK_AS_READ",
+    const expectedAction = {
+      type: MARK_AS_READ,
       index: expectedIndex,
     };
 
-    const result = markAsRead(expectedIndex);
-    expect(result).toEqual(expectedResults);
+    const action = markAsRead(expectedIndex);
+    expect(result).toEqual(expectedAction);
   });
 });
 
 describe("setNotificationFilter", () => {
-  it("Should return expected object with correct type and index as it was called with", () => {
+  it("Should return correct action object when called", () => {
     const expectedResults = {
-      type: "SET_TYPE_FILTER",
+      type: SET_TYPE_FILTER,
       filter: NotificationTypeFilters.DEFAULT,
     };
 
