520c0b3baf918eab99db60ceff3ad740703d0669
joelmuhoho <joekariuki3@gmail.com>
2026-02-13 17:43:08 +0300

    feat: add tests for loginRequest handling success and failure actions

--- 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
@@ -9,7 +9,18 @@ import {
   LOGOUT,
   DISPLAY_NOTIFICATION_DRAWER,
   HIDE_NOTIFICATION_DRAWER,
+  LOGIN_FAILURE,
+  LOGIN_SUCCESS,
 } from "./uiActionTypes";
+import { loginRequest } from "./uiActionCreators";
+import { thunk } from "redux-thunk";
+import fetchMock from "fetch-mock";
+import configureMockStore from "redux-mock-store";
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+const LOGIN_URL = "/dist/login-success.json";
 
 describe("login", () => {
   it("Should return the correct action object when called.", () => {
@@ -53,5 +64,52 @@ describe("hideNotificationDrawer", () => {
 
     const action = hideNotificationDrawer();
     expect(action).toEqual(expectedAction);
+  });
+});
+
+describe("uiActionCreators", () => {
+  it("creates LOGIN and LOGIN_FAILURE when fetching API fails", () => {
+    // Simulate a failure (e.g., 404 or 500 error)
+    fetchMock.get(LOGIN_URL, 500);
+
+    const expectedActions = [
+      {
+        type: LOGIN,
+        user: { user: { email: "test@test.com", password: "password" } },
+      },
+      { type: LOGIN_FAILURE },
+    ];
+
+    const store = mockStore({});
+
+    return store
+      .dispatch(loginRequest("test@test.com", "password"))
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+      });
+  });
+
+  it("creates LOGIN_SUCCESS when fetching API succeeds", () => {
+    fetchMock.get(LOGIN_URL, {
+      headers: { "content-type": "application/json" },
+    });
+
+    const expectedActions = [
+      {
+        type: LOGIN,
+        user: { user: { email: "test@test.com", password: "password" } },
+      },
+      {
+        type: LOGIN_SUCCESS,
+      },
+    ];
+
+    const store = mockStore({});
+
+    return store
+      .dispatch(loginRequest("test@test.com", "password"))
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+      });
   });
 });
